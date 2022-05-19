e362f30046618ad88863ec1e3b839f7fe8cda769
Shutian Lu <lus@uw.edu>
2022-05-19 09:34:28 -0700

    commit all

--- constants.py
+++ constants.py
@@ -1,1 +1,17 @@
 from math import pi
+planck = 6.62607015*1e-34         # J s
+hbar = 6.62607015*1e-34/(2*pi)    # J s
+clight = 299792458                # m/s
+eps0 = 8.854187817*1e-12          # C^2/(J m)
+evolt = 1.602176634*1e-19         # J
+boltzm = 1.380649*1e-23           # J/K
+avogadro = 6.02214076*1e23        # /mol
+rydberg = 10973731.568160         # /m
+echarge = 1.602176634*1e-19       # C
+emass = 9.1093837015*1e-31        # kg
+pmass = 1.67262192369*1e-27       # kg
+amu = 1.66053906660*1e-27         # kg
+a0 = 5.29177210903*1e-11          # m
+gfree = 2.002319304362560         # dimensionless 
+bmagn = 9.274010078300000e-24     # J/Tesla
+mu0 =  1.256637062120000e-06      # N/A^2
